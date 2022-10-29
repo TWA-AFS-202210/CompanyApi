6af5ad83db780059be072c47735ebf85eed3d5a1
Guangguang Ma <ggma@thoughtworks.com>
2022-10-30 00:30:59 +0800

    init commit

---
+++ CompanyApi/Controllers/HelloController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CompanyApi.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class HelloController : ControllerBase
+    {
+        [HttpGet]
+        public string Get()
+        {
+            return "Hello World";
+        }
+    }
+}
